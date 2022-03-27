d285f5c1589c9c0fe4e33e90c0f9a5e32570b5b1
Leonardo Hemming <ldsh@pop-os.localdomain>
2022-03-27 15:03:12 -0300

    Adding Flask and CRUD

--- app/models.py
+++ app/models.py
@@ -1,3 +1,20 @@
-from django.db import models
+from unicodedata import name
+import app
+from dataclasses import dataclass
 
-# Create your models here.
+@dataclass
+class Movie(app.db.Model):
+    
+    id: int
+    title: str
+    url: str
+    
+    id   = app.db.Column(app.db.Integer(), primary_key=True)
+    title = app.db.Column(app.db.String())
+    url  = app.db.Column(app.db.String())
+    
+    def __init__(self,*args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+    def __repr__(self):
+        return f'<Movie id: {self.id} - {self.title}'
